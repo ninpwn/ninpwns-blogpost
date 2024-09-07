92432793311f0da544658bfe820563833f48d60d
ninpwn <anon@gmail.com>
2024-09-07 18:00:46 +0300

    attempt to add about to the other navigation thingy

--- src/config.ts
+++ src/config.ts
@@ -47,6 +47,11 @@ export const profileConfig: ProfileConfig = {
       name: 'GitHub',
       icon: 'fa6-brands:github',
       url: 'https://github.com/ninpwn/',
+    },
+    {
+      name: 'About',
+      icon: '',
+      url: 'https://ninpwn.vercel.app/about/',
     }
   ],
 }
