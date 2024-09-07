b5967c852f18a609a5c04d4f5121fa7ab950ea86
ninpwn <noemail>
2024-09-07 15:43:03 +0300

    changed navbar, removed 'github' link

--- src/config.ts
+++ src/config.ts
@@ -37,12 +37,7 @@ export const navBarConfig: NavBarConfig = {
   links: [
     LinkPreset.Home,
     LinkPreset.Archive,
-    LinkPreset.About,
-    {
-      name: 'GitHub',
-      url: 'https://github.com/saicaca/fuwari',     // Internal links should not include the base path, as it is automatically added
-      external: true,                               // Show an external link icon and will open in a new tab
-    },
+    LinkPreset.About
   ],
 }
 
