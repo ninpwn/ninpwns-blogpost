797463e72a2c32efbdfabeeb27db82e2636a4233
ninpwn <some@email.com>
2025-03-11 21:23:43 +0100

    a

--- src/config.ts
+++ src/config.ts
@@ -43,9 +43,9 @@ export const navBarConfig: NavBarConfig = {
     LinkPreset.Archive,
     LinkPreset.About,
     {
-      name: 'GitHub',
-      url: 'https://github.com/saicaca/fuwari',     // Internal links should not include the base path, as it is automatically added
-      external: true,                               // Show an external link icon and will open in a new tab
+      name: 'About',
+      url: '/about/',     // Internal links should not include the base path, as it is automatically added
+      external: false,                               // Show an external link icon and will open in a new tab
     },
   ],
 }
