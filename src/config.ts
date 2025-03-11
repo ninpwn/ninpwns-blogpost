ce9cc24c768ddb18c2ded0c48a7c4c7aba932af8
ninpwn <some@email.com>
2025-03-11 21:19:55 +0100

    a

--- src/config.ts
+++ src/config.ts
@@ -52,8 +52,8 @@ export const navBarConfig: NavBarConfig = {
 
 export const profileConfig: ProfileConfig = {
   avatar: 'assets/images/demo-avatar.png',  // Relative to the /src directory. Relative to the /public directory if it starts with '/'
-  name: 'Lorem Ipsum',
-  bio: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+  name: 'ninpwn',
+  bio: 'Cyber Security Researcher',
   links: [
     {
       name: 'GitHub',
